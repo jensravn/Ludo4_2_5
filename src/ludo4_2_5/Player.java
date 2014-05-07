bd21ad9f516c3525a8667144ac3f4f96fe0c9e98
jensravn <jensravn@10.25.245.246>
2014-05-07 18:50:00 +0200

    fix numberOfTokensInPlay()

--- src/ludo4_2_5/Player.java
+++ src/ludo4_2_5/Player.java
@@ -5,31 +5,38 @@ package ludo4_2_5;
  * @author jensravn
  */
 public class Player {
-    
+
     String name;
     Token[] token = new Token[4];
     static int counter;
-    
-    /** Player Constructor */
-    Player () {
-        
+
+    /**
+     * Player Constructor
+     */
+    Player() {
+
         counter++;
-        
+
         for (int i = 0; i < 4; i++) {
             token[i] = new Token();
         }
     }
-    
-    /** Checks if player won - all tokens are at the end field*/
-    boolean won () {
+
+    /**
+     * Checks if player won - all tokens are at the end field
+     */
+    boolean won() {
         for (int i = 0; i < 4; i++) {
             if (token[i].finish == false) {
                 return false;
-            } 
+            }
         }
         return true;
     }
-    
+
+    /**
+     * Checks how many tokens the player currently has in play
+     */
     int numberOfTokensInPlay() {
         int numberOfTokensOut = 0;
         for (int i = 0; i < 4; i++) {
