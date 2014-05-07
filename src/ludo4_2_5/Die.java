e69afe104f9aaf60f3e51319a0559c02af568192
jensravn <jensravn@10.25.245.246>
2014-05-07 18:52:15 +0200

    added comments to roll()

--- src/ludo4_2_5/Die.java
+++ src/ludo4_2_5/Die.java
@@ -5,10 +5,12 @@ package ludo4_2_5;
  * @author jensravn
  */
 public class Die {
-    
-    /** Creates a random number between 1 and 6 */
+
+    /**
+     * Method Roll Creates a random number between 1 and 6
+     */
     static int roll() {
-        return (int)(Math.random()*6+1);
+        return (int) (Math.random() * 6 + 1);
     }
-    
+
 }
