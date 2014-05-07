f5ae1e202ad1508aa61856106534a4f735bd79a4
jensravn <jensravn@10.25.245.246>
2014-05-07 18:49:03 +0200

    adding yesOrNo()

--- src/ludo4_2_5/Scan.java
+++ src/ludo4_2_5/Scan.java
@@ -7,27 +7,51 @@ package ludo4_2_5;
 import java.util.Scanner;
 
 public class Scan {
-    
-    /** Input from user - any string */
+
+    /**
+     * Input from user - any string
+     */
     static String str() {
-        
+
         Scanner scan = new Scanner(System.in);
         return scan.nextLine();
     }
-    
-    /** Input from user - between 1 and 4 */
+
+    /**
+     * Input from user - between 1 and 4
+     */
     static int oneToFour() {
-        
+
         String regex = "[1-4]";
         String input = "F";
-        
-        while (!input.matches(regex)){
-        Scanner scan = new Scanner(System.in);
-        input = scan.nextLine();
-        if(!input.matches(regex)){
-            System.out.println("Invalid input. Choose a number from 1 to 4");
-        }
+
+        while (!input.matches(regex)) {
+            Scanner scan = new Scanner(System.in);
+            input = scan.nextLine();
+            if (!input.matches(regex)) {
+                System.out.println("Invalid input. Choose a number from 1 to 4");
+            }
         }
         return Integer.parseInt(input);
+    }
+    
+    /** Input from user - Yes or No */
+    static boolean yesOrNo() {
+
+        String input = "F";
+                
+        while (!input.equals("Y") || !input.equals("N")) {
+            Scanner scan = new Scanner(System.in);
+            input = scan.nextLine().toUpperCase();
+            if(!input.equals("Y")){
+                return true;
+            } else if (!input.equals("N")) {
+                return false;
+            }
+            else {
+                System.out.println("Invalid input. Choose (Y)es or (N)o");
+            }
+        }
+        return false;
     }
 }
