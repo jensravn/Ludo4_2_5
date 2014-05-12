546a25f40239b0820400e2dbcdf24c9ee73758ab
jensravn <jensravn@192.168.54.249>
2014-05-12 18:30:25 +0200

    updated JavaDoc

--- src/ludo4_2_5/Token.java
+++ src/ludo4_2_5/Token.java
@@ -13,42 +13,51 @@ public class Token {
     int state;
 
     /**
-     * Move a token
+     * Moves a token according to the value of the dice Unless token is at
+     * start. Then it moves 1 field.
+     *
+     * @param turn
+     * @param dice
+     * @param board
      */
     void move(int turn, int dice, Board board) {
 
         System.out.print("The token is moved from field " + this.field);
-        if (this.start) { 
-            board.setField(turn, this.field, this.field+1);
+        if (this.start) {
+            board.setField(turn, this.field, this.field + 1);
             this.field += 1;
         } else {
-            board.setField(turn, this.field, this.field+dice);
-            if (this.field+dice >= board.getNumberOfFields()) {
-                this.field = board.getNumberOfFields()-1;
+            board.setField(turn, this.field, this.field + dice);
+            if (this.field + dice >= board.getNumberOfFields()) {
+                this.field = board.getNumberOfFields() - 1;
             } else {
                 this.field += dice;
             }
         }
         System.out.print(" to " + this.field + "\n");
-        
+
         if (this.field == 0) {
             this.setState(0);
-        } else if (this.field >= board.getNumberOfFields()-1) {
+        } else if (this.field >= board.getNumberOfFields() - 1) {
             this.setState(2);
-            System.out.println("!!!!!! PLAYER " + (turn+1) + " GOT A POINT !!!!!!");
+            System.out.println("!!!!!! PLAYER " + (turn + 1) + " GOT A POINT !!!!!!");
         } else {
             this.setState(1);
         }
     }
 
-    /** Capture */
+    /**
+     * Capture - sets the token back to start
+     */
     void capture() {
         this.field = 0;
         setState(0);
     }
-    
+
     /**
      * Return state of the token 0 = start 1 = inPlay 2 = finish
+     *
+     * @return
      */
     int getState() {
         return this.state;
@@ -56,6 +65,8 @@ public class Token {
 
     /**
      * Set state of the token 0 = start 1 = inPlay 2 = finish
+     *
+     * @param state
      */
     void setState(int state) {
         this.state = state;
