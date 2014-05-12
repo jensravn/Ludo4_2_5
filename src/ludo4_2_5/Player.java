2409ba6f1aa927a4a8100d317d4e81f6d33fdb6e
jensravn <jensravn@192.168.54.249>
2014-05-12 12:17:59 +0200

    added quote()

--- src/ludo4_2_5/Player.java
+++ src/ludo4_2_5/Player.java
@@ -46,4 +46,26 @@ public class Player {
         }
         return numberOfTokensOut;
     }
+    
+    void quote() {
+        System.out.print("\n" + name + " says \"");
+        
+        switch ((int)(Math.random()*6)) {
+            case 0:
+                System.out.print("You don't even deserve to be in this game buddy!");break;
+            case 1:
+                System.out.print("And you call yourself a ludo player..?");break;
+            case 2:
+                System.out.print("So we meet again!");break;
+            case 3:
+                System.out.print("Let the battle begin!");break;
+            case 4:
+                System.out.print("Let the gods have mercy on your soul!");break;
+            case 5:
+                System.out.print("Thunder and lightning baby!");break;
+            default:
+                System.out.print("Obey me!");break;
+        }
+        System.out.print("\"\n");
+    }
 }
